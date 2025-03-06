b9b73e6575e82fa41a34f7b5f05aec011d5588f1
Lutho <luthoyrn@gmail.com>
2025-03-06 15:25:13 +0200

    Added README.md and modified lex_msg.py to include usage instructions and file processing functionality.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# Social-Media-Post-Lex-Analyser
+#Social Media Post Lexical Analyser
 
-Performs lexical analysis on files containing messages/posts which you may find in a typical instant messaging or social media application.
+This program performs lexical analysis on files containing messages or posts, similar to those found in instant messaging or social media applications. It identifies words, numbers, whitespace, punctuation, hashtags, name references, and illegal characters.
+
+How to Run?
+
+- Clone the repository:
+  > git clone <your-repo-url>
+- Navigate to the directory containing lex_msg.py:
+  > cd path/to/repo
+
+-Run the lexical analyser with a .msg file as input: > python3 lex_msg.py <filename>.msg
+
+Expected Output
+
+- It saves the tokens to a .tkn file with the same name as the input.
--- lex_msg.py
+++ lex_msg.py
@@ -1,54 +1,61 @@
 import ply.lex as lex
-import ply.yacc as yacc
+import sys
 
-tokens = ['WORD','NUMBER','WHITESPACE','PUNCTUATION','HASHTAG','NAME','ILLEGAL']
+# Define token names
+tokens = ['WORD', 'NUMBER', 'WHITESPACE', 'PUNCTUATION', 'HASHTAG', 'NAME', 'ILLEGAL']
 
+# Token rules
 def t_WHITESPACE(t):
-    r'[ \t]+'
-    t.type = 'WHITESPACE'
+    r'[ \t]+' 
     return t
 
 def t_HASHTAG(t):
-    r'\#[a-zA-Z0-9]+'
-    t.type = 'HASHTAG'
+    r'\#[a-zA-Z0-9]+' 
     return t
 
 def t_NAME(t):
-    r'\@[a-zA-Z0-9]+'
-    t.type = 'NAME'
+    r'\@[a-zA-Z]+'
     return t
 
 def t_WORD(t):
-    r'[a-zA-Z]+'
-    t.type = 'WORD'
+    r'[a-zA-Z]+' 
     return t
 
 def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
+    r'\d+'  
     return t
 
 def t_PUNCTUATION(t):
-    r'[.,;!?]+'
-    t.type = 'PUNCTUATION'
+    r'[.,;!?]'
     return t
 
-
-def t_error(t):
-    t.type = 'ILLEGAL'
+def t_error(t): #ILLEGAL
+    t.type = "ILLEGAL"
+    t.value = t.value[0] 
     t.lexer.skip(1)
     return t
 
+# Building the lexer
 lexer = lex.lex()
 
-while True:
+def process_file(input_filename):
+    output_filename = input_filename.replace(".msg", ".tkn")
+
     try:
-        s = input('text> ')
-    except:
-        break
-    lexer.input(s)
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break
-        print(tok.type,',', tok.value,sep="")
+        with open(input_filename, 'r', encoding='utf-8') as file_in, open(output_filename, 'w', encoding='utf-8') as file_out:
+            for line in file_in:
+                lexer.input(line)
+                while True:
+                    tok = lexer.token()
+                    if not tok:
+                        break
+                    token_output = f"{tok.type},{tok.value}"
+                    file_out.write(token_output + "\n") 
+        print(f"Tokens saved to {output_filename}")
+    except FileNotFoundError:
+        print(f"Error: File '{input_filename}' not found!")
+
+if len(sys.argv) > 1:
+    process_file(sys.argv[1])
+else:
+    print('Usage: python lex_msg.py <filename>.msg')
